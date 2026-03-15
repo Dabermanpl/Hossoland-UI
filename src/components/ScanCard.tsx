03390ad5360e5321f8406c7e875de7770b706cbb
Dabermanpl <dabermanpl@gmail.com>
2026-03-15 11:35:22 +0100

    feat: align ScanCard component with Figma design Node 21:277

--- src/components/ScanCard.tsx
+++ src/components/ScanCard.tsx
@@ -1,12 +1,12 @@
 import ScanIcon from './ScanIcon';
+import './ScanCard.css';
 
 const ScanCard: React.FC = () => {
   return (
     <div className="scan-card">
-      <div className="scan-overlay"></div>
       <div className="scan-content">
         <div className="scan-icon-wrapper">
-          <ScanIcon color="white" size={40} className="scan-brackets-svg" />
+          <ScanIcon color="white" size={48} className="scan-brackets-svg" />
           <div className="scan-line"></div>
         </div>
         <h2 className="scan-title">Dotknij, aby zeskanować</h2>
